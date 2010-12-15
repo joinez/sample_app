609c16e7f8dd3751585913e745cfbbff2227cf6e
Joinez <matthias@scoreloop.com>
2010-12-15 16:47:37 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '240979848eee232d926b051b3e58bdcacaf507ff6e5b515d62bcd2a635f964ed8314a0ba27f50b1fb3765935f702fb11da9554bc3c5d8162941fad908d94f058'

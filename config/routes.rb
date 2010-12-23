0e24aa4f6aa5cc684b1834d8a26b7c24a0113cfd
Joinez <matthias@scoreloop.com>
2010-12-23 12:25:31 +0100

    finished layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
@@ -7,6 +9,8 @@ SampleApp::Application.routes.draw do
   get "pages/about"
   
   get "pages/help"
+  
+  match '/signup',  :to => 'users#new'
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
